202126e53ed8a6900a919cc79bb02c2b6451538e
jdkim <dev.jdkim@gmail.com>
2020-07-05 18:17:44 +0900

    Auth 기능 만들기

--- index.js
+++ index.js
@@ -74,6 +74,20 @@ app.post('/api/users/login', (req, res) => {
 
 app.get('/api/users/auth', auth, (req, res) => {
 
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True 라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true, // role 0 -> 일반유저, role 1 -> 관리자
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+
+
+    })
+
 })
 
 
--- models/User.js
+++ models/User.js
@@ -80,6 +80,24 @@ userSchema.methods.generateToken = function(cb) {
     })
 }
 
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    //토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 다음에
+        //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({"_id": decoded, "token": token}, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user)
+        })
+
+    })
+
+
+}
+
 const User = mongoose.model('User', userSchema) // 스키마를 모델로 감싸준다
 
 module.exports = {User} // 다른 파일에서 사용할 수 있게 해준다
